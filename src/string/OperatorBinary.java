4683d27ef635fd59f236a9743aca85db3b1bb826
fajarsyabanie <62839115+fajarsyabanie@users.noreply.github.com>
2020-04-27 15:38:18 +0800

    Operator Binary Assigment

---
+++ src/string/OperatorBinary.java
@@ -0,0 +1,13 @@
+package string;
+
+public class OperatorBinary {
+
+    //operator (=)  sebagai assigment/penugasan
+    //menempatkan nilai ke dalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama= "Fajar Syabanie";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+}
